96f062862015960b02c76434a530350a34fcbe4f
lorycade <lorenzo.cadente@accenture.com>
2024-12-28 12:33:15 +0100

    add log to check webhook

--- src/app/api/webhook/stripe/route.js
+++ src/app/api/webhook/stripe/route.js
@@ -3,9 +3,11 @@ import Stripe from 'stripe';
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 export async function POST(req) {
+    console.log("reqqqq", req);
+    
     const sig = req.headers['stripe-signature']; // Firma del webhook
     const buf = await req.text(); // Ottieni il buffer della richiesta
-    
+
     let event;
 
     try {

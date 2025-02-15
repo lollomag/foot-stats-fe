142c0a99cd5fab9ff910b84ef9550d0eca878cb2
lorycade <lorenzo.cadente@accenture.com>
2025-02-15 12:44:45 +0100

    build fixes 2

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,9 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/Header";
 import { UserProvider } from "@/context/UserContext";
 import CustomBreadcrumb from "@/components/CustomBreadcrumb";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
